131eae001fc22fb7fd98d39f8cafa641167675f1
uekkee <uekkee@gmail.com>
2020-07-24 12:50:06 +0900

    fix :cop:

--- babel.config.js
+++ babel.config.js
@@ -29,10 +29,10 @@ module.exports = function(api) {
         '@babel/preset-env',
         {
           targets: {
-            "edge": "17",
-            "firefox": "60",
-            "chrome": "67",
-            "safari": "11.1",
+            edge: '17',
+            firefox: '60',
+            chrome: '67',
+            safari: '11.1',
           },
           useBuiltIns: 'usage',
           corejs: 3,
